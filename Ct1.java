fb66d0ff28c4a6d4471b53a303a95007623960a9
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-10-31 18:42:35 +0900

    ct1

---
+++ Ct1.java
@@ -0,0 +1,16 @@
+public class Ct1 {
+
+    public static double solution(int[] arr) {
+        double answer = 0;
+        for(int i=0;i<arr.length;i++){
+            answer+=arr[i];
+        }
+        return answer/arr.length;
+    }
+
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4};
+        System.out.println(solution(arr));
+    }
+}
