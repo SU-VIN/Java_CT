eccdf18f48cfbee4a6e70cb7813c606980eeba66
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-06 00:30:07 +0900

    ct3

---
+++ Ct3.java
@@ -0,0 +1,14 @@
+public class Ct3 {
+    public static long solution(int a, int b) {
+        long answer = 0;
+        for(int i=Math.min(a,b);i<=Math.max(a,b);i++){
+            answer+=i;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int a=3;
+        int b=5;
+        System.out.println(solution(a,b));
+    }
+}
