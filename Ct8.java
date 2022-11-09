1d2ebcadd02e743850ed7724453d151bf7123948
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-09 19:46:53 +0900

    ct8

--- Ct8.java
+++ Ct8.java
@@ -1,5 +1,25 @@
-public class Ct8 {
-    public static void main(String[] args) {
+//짝지어 제거하기
+import java.util.Stack;
 
+public class Ct8 {
+
+    public int solution(String s)
+    {
+        Stack<Character> result = new Stack<Character>(); // size == 0
+        for(int i=0;i< s.length();i++){ // length = 6 // 012345
+            char c = s.charAt(i);
+            if(!result.isEmpty()&&result.peek()==c){
+                result.pop();
+            }else {
+                result.push(c);
+            }
+
+        }
+        return result.size()==0?1:0;
+    }
+    public static void main(String[] args) {
+        Ct8 sol = new Ct8();
+        String s ="baabaa";
+        System.out.println(sol.solution(s));
     }
 }
