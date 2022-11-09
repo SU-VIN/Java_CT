3b540f4f634acfbe26e1759427ed28abd55c0d9f
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-09 13:01:44 +0900

    ct6

--- Ct6.java
+++ Ct6.java
@@ -1,5 +1,22 @@
+//삼총사
 public class Ct6 {
+
+    public static int solution(int[] number) {
+        int answer = 0;
+        for(int i=0;i<number.length;i++){
+            for(int j=i+1;j<number.length;j++){
+                for(int l=j+1;l<number.length;l++){
+                    if(number[i]+number[j]+number[l]==0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args) {
+        int[] numbers = {-3, -2, -1, 0, 1, 2, 3};
+        System.out.println(solution(numbers));
 
     }
 }
