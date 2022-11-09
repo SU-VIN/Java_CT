34e7f900f643d37e61c009ef22662353664713b4
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-09 13:19:07 +0900

    ct7

--- Ct7.java
+++ Ct7.java
@@ -1,5 +1,24 @@
-public class Ct7 {
-    public static void main(String[] args) {
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Ct7 {
+    public static int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i=0;i<A.length;i++){
+            answer+=A[i]*B[A.length-1-i];
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int []A ={1,4,2};
+        int []B={5,4,4};
+        System.out.println(solution(A,B));
     }
 }
