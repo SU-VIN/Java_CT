83abc666510d2e338f19862365d47b56fa44d7f0
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-09 20:07:30 +0900

    ct9

--- Ct9.java
+++ Ct9.java
@@ -1,6 +1,14 @@
+//핸드폰 번호 가리기
 public class Ct9 {
-    public static void main(String[] args) {
-
+    public String solution(String phone_number) {
+        return "*".repeat(phone_number.length()-4)+phone_number.substring(phone_number.length()-4,phone_number.length());
     }
+    public static void main(String[] args) {
+        Ct9 sol = new Ct9();
+        String pn = "01033334444";
+        System.out.println(sol.solution(pn));
+    }
+
+
 }
 
