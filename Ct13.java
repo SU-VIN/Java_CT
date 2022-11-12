747f09ab8d7d5560eeb56aa79c81c0f53e1a6ad0
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-12 19:19:21 +0900

    ct13

--- Ct13.java
+++ Ct13.java
@@ -1,7 +1,32 @@
 public class Ct13 {
+    public String solution(String s, int n) {
+        String answer = "";
+        //A = 65 Z =90 a=97 z=122
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='A'&&s.charAt(i)<='Z'){
+                if(s.charAt(i)+n>90){
+                    answer+=(char)(s.charAt(i)+n-26);
+                }else {
+                    answer += (char) (s.charAt(i) + n);
+                }
+
+            } else if (s.charAt(i)>='a'&&s.charAt(i)<='z') {
+                if(s.charAt(i)+n>122){
+                    answer+=(char)(s.charAt(i)+n-26);
+                }else {
+                    answer += (char) (s.charAt(i) + n);
+                }
+            }else{
+                answer+=" ";
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args) {
         Ct13 sol = new Ct13();
-        System.out.println();
+        String s = "a B z";
+        int n=4;
+        System.out.println(sol.solution(s,n));
 
     }
 }
