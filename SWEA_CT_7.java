682bfc13e250a82c93929fbe06b99c5eada6467c
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-17 19:08:22 +0900

    ct7

--- SWEA_CT_7.java
+++ SWEA_CT_7.java
@@ -1,3 +1,4 @@
+//간단한 369게임
 import java.util.Scanner;
 
 public class SWEA_CT_7 {
@@ -6,14 +7,34 @@ public class SWEA_CT_7 {
         Scanner sc = new Scanner(System.in);
         int T;
         T=sc.nextInt();
-
+        char temp;
+        String num;
+        String result ="";
+        int count =0;
         for(int i=1;i<=T;i++)
         {
+            num=Integer.toString(i);
 
+            for(int j=0;j<num.length();j++){
+                temp=num.charAt(j);
+                if((int)temp%3==0&&temp!='0'){
+                    count ++;
+                }
+            }
+            if(count>0){
+                for(int k=0;k<count;k++){
+                    result+="-";
+                }
+            }else{
+                result+=num;
+            }
+
+            result+=" ";
+            count =0;
 
         }
-        System.out.println(396%10);
-        System.out.println(396/10);
+        System.out.println(result);
+
 
     }
 }
--- javatest.java
+++ javatest.java
@@ -1,5 +1,7 @@
 public class javatest {
     public static void main(String[] args) {
 //쉬자
+        String[] arr = {"hi","heool"};
+        System.out.println(arr[0]);
     }
 }
