d0e3d492e84cb559313a6e6b50312bf2c28f330f
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-12-04 20:53:41 +0900

    BJ_CT_9 update

--- BJ_CT_9.java
+++ BJ_CT_9.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 import java.util.Stack;
-
+//그룹 단어 체커 -문자열- 실버5
 public class BJ_CT_9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int result =0;
-        Stack<Character> st = new Stack<>();
+        int result = 0;
+        int flag =0;
 
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             String s = "";
             String str = sc.next();
+            Stack<Character> st = new Stack<>();
             st.push(str.charAt(0));
-            for(int k=1;k<str.length();k++){
-                if(st.peek()!=str.charAt(k)) st.push(str.charAt(k));
+            for (int k = 1; k < str.length(); k++) {
+                if (st.peek() != str.charAt(k)) st.push(str.charAt(k));
             }
 
-            for(int l=0;l<st.size();l++) s+=st.get(l);
+            for (int l = 0; l < st.size(); l++) s += st.get(l);
 
-            for(int j=0;j<s.length();j++){
-                if(s.indexOf(s.charAt(j),j+1)>=0) {
+
+            for (int j = 0; j < s.length(); j++) {
+                if(s.indexOf(s.charAt(j),j+1)>=0){//포함 그룹단어 아님
                     result++;
                     break;
                 }
-
             }
-        }
 
-        System.out.println(result);
-    }
-}
+        }
+        System.out.println(n-result);
+    }}
+
