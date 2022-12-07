30f24018c46bae293e4291824b72b959bbc15d04
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-12-07 22:06:21 +0900

    BJ_CT_10 correct

--- BJ_CT_10.java
+++ BJ_CT_10.java
@@ -4,41 +4,51 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//세그먼트 트리
+//세그먼트 트리 이분탐색- 실버2- 숫자카드
 public class BJ_CT_10 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n =Integer.parseInt(br.readLine());
         int[] narr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder str = new StringBuilder();
 
         for(int i=0;i<n;i++){
             narr[i]=Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(narr); //정렬
+
         int m =Integer.parseInt(br.readLine());
-        int[] marr= new int[m];
         st= new StringTokenizer(br.readLine());
 
         for(int i=0;i<m;i++){
-            marr[i]=Integer.parseInt(st.nextToken());
-        }
+            int left = 0;
+            int right=n-1;
+            int num =Integer.parseInt(st.nextToken());
 
-        int[] check = new int[m];
-        Arrays.fill(check,0);
+            while (left<=right){
+                int midindex = (left+right)/2;
+                int middle = narr[midindex];
 
-        String str= "";
-//--------------------입력처리-----------------------------------
+                if (num==middle){
+                    str.append(1+" ");
+                    break;
+                }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(marr[i]==narr[j]) check[i]=1;
+                if(num<middle) right=midindex-1;
+                else if(num>middle) left=midindex+1;
+
+                if(left>right){
+                    str.append(0+" ");
+                    break;
+                }
             }
+
+
         }
 
-        for(int i=0;i<check.length;i++){
-            str+=check[i]+" ";
-        }
+
         System.out.println(str);
     }
 }
