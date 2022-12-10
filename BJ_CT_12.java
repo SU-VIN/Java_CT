5cae358ff009f3a95fd7cb476c0fc7e13c1531b4
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-12-10 14:39:09 +0900

    BJ_CT_12

--- BJ_CT_12.java
+++ BJ_CT_12.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//실버3 - 예산- 이분탐색
 public class BJ_CT_12 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
