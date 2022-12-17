027f0d77e14b13f4331f0ba626ecb2d55f16431f
SU_VIN <pyotnqls25@naver.com>
2022-12-17 18:24:14 +0900

    Update PRO_CT_3.java

--- PRO_CT_3.java
+++ PRO_CT_3.java
@@ -7,7 +7,7 @@ public class PRO_CT_3 {
     public static int[] solution(int N, int[] stages) {
         double[] answer = new double[N];
         int[] result =new int[N];
-        Map<Integer,Double> map = new HashMap();
+        Map<Integer,Double> map = new HashMap(); //key=스테이지번호 value=실패율
 
         for(int i=1;i<=N;i++){
             double failureLate = 0;//실패율
@@ -22,11 +22,11 @@ public class PRO_CT_3 {
                 }
             }
             map.put(i,a/b);
-            if(a==0&&b==0) map.put(i,0.0);
+            if(a==0&&b==0) map.put(i,0.0); //0/0은 NaN이 뜨기때문에 강제로 0을 넣어줌
         }
 
         List<Integer> keySet = new ArrayList<Integer>(map.keySet());
-        keySet.sort((o1,o2)-> map.get(o2).compareTo(map.get(o1)));
+        keySet.sort((o1,o2)-> map.get(o2).compareTo(map.get(o1)));//value값으로 내림차순 정렬
         for (int i=0;i<N;i++) {
             result[i]=keySet.get(i);
         }
