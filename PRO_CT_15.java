b2601cf0f40d2888d24a14396adb164ff2705a32
SU_VIN <pyotnqls25@naver.com>
2023-01-15 23:36:43 +0900

    PRO_CT_15

--- PRO_CT_15.java
+++ PRO_CT_15.java
@@ -1,39 +1,58 @@
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
+//레벨 4 카카오 행렬과 연산
 public class PRO_CT_15 {
-    public static int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        int[] arr= new int[progresses.length];
-        Stack<Integer> st = new Stack<>();
-        int count =1;
+    public static int[][] solution(int[][] rc, String[] operations) {
+        int[][] answer = new int[rc.length][rc[0].length];
+        ArrayDeque<Integer>left = new ArrayDeque<>();
+        ArrayDeque<Integer>right = new ArrayDeque<>();
+        ArrayDeque<ArrayDeque<Integer>>mid = new ArrayDeque<>();
 
-        for(int i=0;i<arr.length;i++){
-            arr[i]=(int)Math.ceil((100-progresses[i])/speeds[i]);
+        //큐에 데이터 받기
+        //O(N) N=r*c = 100,000
+        for(int i=0;i<rc.length;i++){
+            left.addLast(rc[i][0]);
+            ArrayDeque<Integer> midQueue = new ArrayDeque<>();
+            for(int j=1;j<rc[0].length-1;j++){
+                midQueue.addLast(rc[i][j]);
+
+            }
+            mid.addLast(midQueue);
+            right.addLast(rc[i][rc[i].length-1]);
         }
 
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]<arr[j]){
-                    st.push(count);
-                    count=1;
-                    break;
-                }else{
-                    count++;
-                }
+        //O(N) N=100,000
+        for(String operation:operations){
+            if("ShiftRow".equals(operation)){
+                left.addFirst(left.pollLast()); //O(1)
+                mid.addFirst(mid.pollLast());//O(1)
+                right.addFirst(right.pollLast());//O(1)
+
+            }else{
+                mid.peekFirst().addFirst(left.pollFirst());//O(1)
+                right.addFirst(mid.peekFirst().pollLast());//O(1)
+                mid.peekLast().addLast(right.pollLast());//O(1)
+                left.addLast(mid.peekLast().pollFirst());//O(1)
             }
         }
 
-        int[] result = new int[st.size()];
-        for(int i=0;i<result.length;i++){
-            result[i]=st.get(i);
+
+        for(int i=0;i<answer.length;i++){
+            answer[i][0]=left.pollFirst();
+            ArrayDeque<Integer> midqueue=mid.pollFirst();
+            for(int j=1;j<answer[i].length-1;j++){
+                answer[i][j]=midqueue.pollFirst();
+            }
+            answer[i][answer[i].length-1]=right.pollFirst();
         }
-        return result;
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        int[] progresses = {93,39,50};
-        int[] speeds = {1,30,5};
-        System.out.println(solution(progresses,speeds));
+        int[][]rc = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        String[] operatoins = {"Rotate", "ShiftRow"};
+
+        System.out.println(solution(rc,operatoins));
     }
 }
