a21f8908a988bd6186b976ccdb3a82a46ea6272a
SU_VIN <pyotnqls25@naver.com>
2023-01-29 21:42:32 +0900

    Update BJ_CT_14.java
    오큰수 다시풀기

--- BJ_CT_14.java
+++ BJ_CT_14.java
@@ -1,39 +1,70 @@
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.Stack;
 import java.util.StringTokenizer;
 //골드4 오큰수
 public class BJ_CT_14 {
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int N = Integer.parseInt(br.readLine());
+//        int [] arr = new int[N];
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        for(int i=0;i<N;i++){
+//            arr[i]=Integer.parseInt(st.nextToken());
+//        }
+//
+//        Stack<Integer> stack = new Stack<>();
+//        for(int i=0;i<N;i++){
+//            while(!stack.isEmpty()&&arr[stack.peek()]<arr[i]){
+//                arr[stack.pop()]=arr[i];
+//            }
+//            stack.add(i);
+//        }
+//
+//        while (!stack.isEmpty()){
+//            arr[stack.pop()]=-1;
+//        }
+//
+//        for(int i=0;i<N;i++){
+//            bw.write(arr[i]+" ");
+//        }
+//
+//
+//        bw.flush();
+//
+//
+//    }
+    //다시풀기
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int [] arr = new int[N];
+
+        int[]num = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i]=Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<N;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
         }
 
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<N;i++){
-            while(!stack.isEmpty()&&arr[stack.peek()]<arr[i]){
-                arr[stack.pop()]=arr[i];
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int[]answer=new int[N];
+
+        //뒤부터 탐색
+        for(int i=N-1;i>=0;i--){
+            while (!stack.isEmpty()&&stack.peekLast()<=num[i]){
+                stack.pollLast();
             }
-            stack.add(i);
+            answer[i]=stack.isEmpty()?-1:stack.peekLast();
+            stack.addLast(num[i]);
         }
 
-        while (!stack.isEmpty()){
-            arr[stack.pop()]=-1;
+        for(int i:answer){
+            bw.write(i+" ");
         }
-
-        for(int i=0;i<N;i++){
-            bw.write(arr[i]+" ");
-        }
-
-
         bw.flush();
-
-
-    }
+}
 }
