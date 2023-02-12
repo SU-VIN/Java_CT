9d0acec01a9dd3b12c1eac7e1678e266b9216954
SU_VIN <pyotnqls25@naver.com>
2023-02-12 20:38:54 +0900

    BJ_CT_15

---
+++ PRO_CT_23.java
@@ -0,0 +1,14 @@
+public class PRO_CT_23 {
+    //디펜스 게임
+    public static int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n=7;
+        int k=3;
+        int[]enemy = {4, 2, 4, 5, 3, 3, 1};
+        System.out.println(solution(n,k,enemy));
+    }
+}
---
+++ PRO_CT_24.java
@@ -0,0 +1,14 @@
+public class PRO_CT_24 {
+    //징검다리 건너기
+    public static int solution(int[] stones, int k) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+        int k=3;
+        System.out.println(solution(stones,k));
+    }
+
+
+}
