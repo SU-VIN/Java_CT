4e617216d70cc70a404ac100353bb23e5b53ee45
SU_VIN <pyotnqls25@naver.com>
2023-03-06 11:48:18 +0900

    PRO_CT_34

--- PRO_CT_34.java
+++ PRO_CT_34.java
@@ -1,10 +1,36 @@
+import java.util.PriorityQueue;
+
 public class PRO_CT_34 {
-    //레벨2 부대복귀
-    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        int[] answer = {};
+    //레벨2 더 맵게
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+
+        if(pq.peek()>=K) return answer; //처음부터 완성인경우
+
+        int min = pq.peek();
+        while (min<K && pq.size()>1){
+            int num1=pq.poll();
+            int num2=pq.poll();
+            int sum = num1+(num2*2);
+            pq.add(sum);
+            min=pq.peek();
+
+            answer++;
+
+        }
+
+        if(min<K) return -1;
+
         return answer;
     }
     public static void main(String[] args) {
-
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int k=7;
+        System.out.println(solution(scoville,k));
     }
 }
