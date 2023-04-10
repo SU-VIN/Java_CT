d063759bd5442cd72fb999a7047f452faf33f85f
SU_VIN <pyotnqls25@naver.com>
2023-04-10 19:05:08 +0900

    PRO_CT_40

---
+++ PRO_CT_40.java
@@ -0,0 +1,36 @@
+public class PRO_CT_40 {
+    //레벨2 스킬트리
+    public static int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for (int i = 0; i < skill_trees.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            int cnt=0;
+            //skill 문자 제외 문자는 무시함 ex) BACDE -> BCD
+            for(int j=0;j<skill_trees[i].length();j++){
+                
+                if(skill.contains(skill_trees[i].substring(j,j+1))){
+                    sb.append(skill_trees[i].substring(j,j+1));
+                }else{
+                    cnt++;
+                }
+            }
+            if(cnt==skill_trees[i].length()) answer++;
+
+            //skill이 될 수 있는지 확인
+            for(int j=0;j<sb.length();j++){
+                String str = skill.substring(0, sb.length());
+                if(str.equals(sb.toString())){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String skill = "CBD";
+        String[] skill_trees={"BACDE", "CBADF", "AECB", "BDA"};
+        System.out.println(solution(skill, skill_trees));
+    }
+}
--- javatest.java
+++ javatest.java
@@ -1,27 +1,66 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class javatest {
+
+    public static int solution(int n, int m, int k, int[] u, int[] v, int[] l) {
+        int answer = 0;
+        int max = Integer.MAX_VALUE;
+        u = Arrays.copyOfRange(u,0,n);
+        v = Arrays.copyOfRange(v,0,n);
+
+        //컨베이너 벨트가 이어져있는지 확인
+        for (int i = 1; i <= n; i++) {
+            if(u[i-1]!=i&&v[i-1]!=i){
+                return -1;
+            }
+        }
+
+        //벨트 길이 최솟값 구하기
+        boolean[] visited = new boolean[l.length];
+        choice(l,visited,0,n);
+
+
+        
+
+
+        return answer;
+    }
+
+    static void choice(int[] arr, boolean[] visited, int start, int r){
+        if(r==0){
+            return
+        }else{
+            for (int i = 0; i < arr.length; i++) {
+                visited[i]=true;
+                choice(arr,visited,i+1,r-1);
+                visited[i]=false;
+            }
+        }
+    }
+
+    static int[] print(int[] arr, boolean[] visited) {
+        for(int i = 0; i < arr.length; i++) {
+            if(visited[i] == true)
+                System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
+        int n=4;
+        int m=6;
+        int k=9;
+        int[] u = {1,2,3,1,1,2};
+        int[] v ={2,3,4,4,3,4};
+        int[] l ={3,3,3,3,1,2};
+//        int n=3;
+//        int m=3;
+//        int k=1;
+//        int[] u = {1,1,2};
+//        int[] v ={2,3,3};
+//        int[] l ={2,2,2};
+        System.out.println(solution(n,m,k,u,v,l));
 
-//멀미나서 못하겠어요
-//쉬자ㅋㅇㄴㅇㅣㅏㅓㅣㅙㅑㅗㅑㅗㅣㅘㅓㅏshwuhd
-        String[] arr1 = {"나","가","다"};
-        int [] arr2 = {3,4,2,5,1};
-        Integer[] arr3 = new Integer[arr2.length];
-        for(int i=0;i<arr3.length;i++){
-            arr3[i]=arr2[i];
-        }
-
-//        Arrays.sort(arr1, Collections.reverseOrder());
-        Arrays.sort(arr3,Collections.reverseOrder());
-//
-//        for(String x :arr1){
-//            System.out.println(x);
-//        }
-
-        for(int x :arr3){
-            System.out.println(x);
-        }
     }
 }
