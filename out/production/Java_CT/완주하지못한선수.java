fa1babcd740e76ff40f2c71a3447a1f4ecc3e089
SU_VIN <pyotnqls25@naver.com>
2023-09-16 14:29:33 +0900

    완주하지못한선수
    hashmap getOrDefault는 +1만 할수있는게 아니다

---
+++ out/production/Java_CT/완주하지못한선수.java
@@ -0,0 +1,34 @@
+package out.production.Java_CT;
+
+import java.util.HashMap;
+
+public class 완주하지못한선수 {
+
+        public static String solution(String[] participant, String[] completion) {
+            String answer = "";
+            HashMap<String,Integer>hash = new HashMap<String,Integer>();
+
+            for(String name : participant){
+                hash.put(name,hash.getOrDefault(name,0)+1);
+            }
+
+
+            for(String name : completion){
+                hash.put(name,hash.getOrDefault(name,0)-1);
+            }
+
+            for(String name : hash.keySet()){
+                if(hash.get(name).equals(1)){
+                    return name;
+                }
+            }
+            System.out.println(hash);
+            return answer;
+        }
+    
+    public static void main(String[] args) {
+        String[] participant={"mislav", "stanko", "mislav", "ana"};
+        String[]completion={"stanko", "ana", "mislav"};
+        System.out.println(solution(participant, completion));
+    }
+}
